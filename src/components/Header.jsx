f2ee82003ab456d32fde89f1bc6a07ac97022c81
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-11 15:58:41 +0800

    Added something in header. To edit later.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
-export default function Header() {
+export default function Header({children, isHeader=true}) {
+    const size = isHeader ? "text-4xl" : "text-2xl"
     return (
         <>
+        <h6 className={size}>{children}</h6>
         </>
     )
 }
