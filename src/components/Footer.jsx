e2513d2fa245cc1c32a206f742602ef770935797
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-13 10:27:51 +0800

    Added links to Footer. Still needs to finalize it.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,11 @@
+import NavLinks from "./Navlinks"
 export default function Footer() {
     return (
         <>
+        <div className="fixed bottom-0 w-full bg-white/95 shadow-lg h-18">
+        <NavLinks position="justify-center fixed bottom-8"/>
+        <p className="w-full flex justify-center fixed bottom-2 text-black/50">Copyright © 2025 Princess Giron. All Rights Reserved.</p>
+        </div>
         </>
     )
 }
