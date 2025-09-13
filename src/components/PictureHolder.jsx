a24d2bd7fcfb2fec682d8b50123aface8ffcf767
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-13 08:41:08 +0800

    Created a customizable PictureHolder component. Width can be edited and drop shadow can be added.

--- src/components/PictureHolder.jsx
+++ src/components/PictureHolder.jsx
@@ -1,6 +1,11 @@
-export default function PictureHolder() {
+export default function PictureHolder({imgSrc="", alt="", isBorderRound=true, isDrop=false, dropOnHover=false,width="max-w-[150px]"}) {
+    const baseStyle = ""
+    const dropShadow = isDrop ? "drop-shadow-md" : ""
+    const dropRightAway = dropOnHover ? `animation ease-in-out hover:drop-shadow-md hover:-translate-y-1 ` : dropShadow
+    const round = isBorderRound ? "rounded-lg" : ""
     return (
         <>
+        <img src={imgSrc} alt={alt} className={`${baseStyle} ${round} ${width} ${dropRightAway}`}/>
         </>
     )
 }
