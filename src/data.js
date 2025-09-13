d8c27ee3f5b35183613f820ce837baa9b45eb1d3
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-13 18:08:54 +0800

    Added some information in data.js. Would add more stuff later.

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const organization = [
+    {
+        name: "Ateneo Gabay",
+        activities: [
+            {
+                role: "Deputy for Website Management",
+                description: "",
+                year: 2025,
+            },
+            {
+                role: "Salubong 2025 Core Team",
+                description: "",
+                year: 2025,
+            },
+        ]
+        
+    },
+    {
+        name: "Google Developer Students Club-Loyola",
+        activities: [
+            {
+                role: "Financial Officer",
+                description: "",
+                year: 2025,
+            },
+        ]
+        
+    },
+    
+]
+export const education = [
+    {
+        name: "Ateneo De Manila University",
+        program: "Bachelor of Computer Science with Specialization in Data Science and Analytics",
+        year: "2023-2027",
+        description: "",
+    },
+    {
+        name: "Montessori De Sagrada Familia",
+        program: "Science Technology and Mathematics Strand",
+        year: "2017-2023",
+        description: "",
+    },
+]
+
+export const certification = [
+    {
+        name: "",
+        year: "",
+    },
+]
