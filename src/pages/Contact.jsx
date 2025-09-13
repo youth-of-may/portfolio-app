c9417b9452ff4ad9b61f6d9d8841f2a580cccad2
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-13 18:09:34 +0800

    Added initial content for Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,12 @@
+import Header from "../components/Header"
+import Subheader from "../components/Subheader"
 export default function Contact() {
     return (
         <>
+        <div className="text-center flex flex-col">
+            <Header isHeader={true} misc="mb-4">Contact Me</Header>
+            
+        </div>
         </>
     )
 }
