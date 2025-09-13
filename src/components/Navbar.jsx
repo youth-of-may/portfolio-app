b53c891a510d9bc7b26d13d841378936e2dc6737
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-13 10:28:15 +0800

    Fixed Navbar to use the NavLink component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom";
+import NavLinks from "./Navlinks";
+import Button from "./Button";
+
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="/">About</Link>
-      <Link to="/experience">Experience</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/contacts">Contact</Link>
-    </nav>
+
+    <div className="mt-4 mb-16 flex items-center">
+    <Button px="px-8 text-md font-semibold">PRINCESS GIRON</Button>
+    <NavLinks/>
+    </div>
   );
 }
