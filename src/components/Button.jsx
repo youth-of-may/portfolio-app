e8ddb4cc71de167ec999851423c53b95b5c0de16
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-13 08:40:13 +0800

    Created a customizable Button component.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,26 @@
-export default function Button({children, isRound=true}) {
-    const classStyle= isRound ? "" : ""
-    return (
-        <>
-        <button className={classStyle}>{children}</button>
-        </>
-    )
+export default function Button({
+  children,
+  px = "px-16",
+  hoverBg = "hover:bg-[#10549f]",
+  isRound = true,
+  url,
+}) {
+  const baseStyle =
+    "py-2 border border-black transition ease-in-out hover:text-white hover:scale-105";
+  const classStyle = isRound ? "rounded-full" : "rounded-md";
+
+  function handleClick() {
+    if (url) {
+      window.open(url, "_blank");
+    }
+  }
+
+  return (
+    <button
+      className={`${px} ${hoverBg} ${classStyle} ${baseStyle}`}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
 }
