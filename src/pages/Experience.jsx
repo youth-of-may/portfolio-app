2ec699876b29192eea318cfad44df9d34fccdddf
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-13 10:29:39 +0800

    Added dummy picture for checking.

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,6 +1,9 @@
+import PictureHolder from "../components/PictureHolder"
 export default function Experience() {
     return (
         <>
+        <PictureHolder imgSrc="https://i.pinimg.com/736x/25/98/2c/25982c2af2cca84c831a37dedfd15c66.jpg" 
+        alt="randomImage" isDrop={true} dropOnHover={true}/>
         </>
     )
 }
