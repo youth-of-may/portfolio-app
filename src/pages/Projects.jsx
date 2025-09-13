6b8a30459615fc0146cf1e9f09f6246f2f491c39
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-13 10:29:56 +0800

    Added dummy button for checking.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,8 @@
+import Button from "../components/Button"
 export default function Projects() {
     return (
         <>
+        <Button isRound={true} url="">Project1</Button>
         </>
     )
 }
