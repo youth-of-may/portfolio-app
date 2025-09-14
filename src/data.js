aa3afb8ddf4207df98676262fa91a6b9ed7ec75c
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-14 08:08:49 +0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,18 +1,23 @@
+import ateneo from "./assets/ateneo.png";
+import gabay from "./assets/gabay.png";
+import gdsc from "./assets/gdsc.png";
+import mdsf from "./assets/mdsf.png";
 export const organization = [
     {
         name: "Ateneo Gabay",
         activities: [
             {
                 role: "Deputy for Website Management",
-                description: "",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula velit.",
                 year: 2025,
             },
             {
                 role: "Salubong 2025 Core Team",
-                description: "",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula velit.",
                 year: 2025,
             },
-        ]
+        ],
+        url: gabay
         
     },
     {
@@ -20,10 +25,11 @@ export const organization = [
         activities: [
             {
                 role: "Financial Officer",
-                description: "",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula velit.",
                 year: 2025,
             },
-        ]
+        ],
+        url: gdsc
         
     },
     
@@ -34,12 +40,14 @@ export const education = [
         program: "Bachelor of Computer Science with Specialization in Data Science and Analytics",
         year: "2023-2027",
         description: "",
+        url: ateneo
     },
     {
         name: "Montessori De Sagrada Familia",
         program: "Science Technology and Mathematics Strand",
         year: "2017-2023",
         description: "",
+        url: mdsf
     },
 ]
 
@@ -48,4 +56,12 @@ export const certification = [
         name: "",
         year: "",
     },
+]
+
+export const projects = [
+    {
+        name: "",
+        description: "",
+        link: "",
+    }
 ]
