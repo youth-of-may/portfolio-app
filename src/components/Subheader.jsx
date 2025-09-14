271ad0b19d213700d78bf1a90779c66c2ea2bfdb
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-14 08:08:39 +0800

    Created a size parameter.

--- src/components/Subheader.jsx
+++ src/components/Subheader.jsx
@@ -1,5 +1,5 @@
-export default function Subheader({children, color="text-black", weight="font-regular", misc="mb-12", handleClick}) {
-    const baseStyle = `${color} ${weight} ${misc} text-xl `
+export default function Subheader({children, color="text-black", size="text-xl", weight="font-regular", misc="mb-12", handleClick}) {
+    const baseStyle = `${color} ${weight} ${misc} ${size} `
     return (
         <>
         <h6 className={`${baseStyle}`} onClick={handleClick}>{children}</h6>
