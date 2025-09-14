f38535626ef3608e28fcf4c267d157cfec4dc51a
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-14 08:07:57 +0800

    Added info about education.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,7 +1,21 @@
 import { education } from "../data"
+import Subheader from "./Subheader"
+import PictureHolder from "./PictureHolder"
+import Header from "./Header"
 export default function Education() {
     return (
         <>
+        {education.map((educ) =>
+                <div className="flex w-full mb-4 border border-gray-400/50 rounded-md p-4 bg-white hover:drop-shadow-gray-400/30 hover:drop-shadow-lg hover:translate-y-1">
+                    <PictureHolder imgSrc={educ.url} width="max-w-[225px] mr-8"></PictureHolder>
+                    <div className="flex flex-col my-auto">
+                    <Header isHeader={false} misc=" text-left mb-2">{educ.name}</Header>
+                    <Subheader misc="text-left">{educ.program}</Subheader>
+                    <Subheader misc="text-left" size="text-lg" color="text-gray-500">{educ.year}</Subheader>
+                    <Subheader misc="text-left" size="text-md" color="text-gray-500">{educ.description}</Subheader>
+                    </div>
+                </div>
+         )}
         </>
     )
 }
