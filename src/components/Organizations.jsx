4cd3ffacd30d863d3346c771f6a10d564ab603ed
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-14 16:36:17 +0800

    Made it more responsive to small screen sizes.

--- src/components/Organizations.jsx
+++ src/components/Organizations.jsx
@@ -6,13 +6,13 @@ export default function Organizations() {
     return (
         <>
         {organization.map((org) =>
-        <div className="flex w-full mb-4 border border-gray-400/50 rounded-md p-4 bg-white hover:drop-shadow-gray-400/30 hover:drop-shadow-lg hover:translate-y-1">
-            <PictureHolder imgSrc={org.url} width="max-w-[225px] mr-8"></PictureHolder>
+        <div className=" flex mb-4 border border-gray-400/50 rounded-md p-4 bg-white hover:drop-shadow-gray-400/30 hover:drop-shadow-lg hover:translate-y-1">
+            <PictureHolder imgSrc={org.url} width="max-w-[150px] mr-8"></PictureHolder>
             <div className="flex flex-col my-auto">
             <Header isHeader={false} misc=" text-left mb-2">{org.name}</Header>
             <div className="flex flex-col">
             {org.activities.map((act) =>
-            <div className="flex flex-col">
+            <div className="flex flex-col ">
                 
                 <Subheader misc="text-left">{act.role}</Subheader>
                 <Subheader misc="text-left" size="text-lg" color="text-gray-500">{act.description}</Subheader>
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 export default function MainLayout() {
   return (
     <>
-    <main className="px-8 mx-auto max-w-6xl">
+    <main className="px-8 mx-auto lg:max-w-6xl md:max-w-3xl sm:max-w-xl">
       <Navbar />  
         {/* Child routes render here */}
         <Outlet />
