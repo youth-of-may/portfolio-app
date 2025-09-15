247b2a8f8898488a87081d6ab4520e82c57cbd6f
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-15 08:14:36 +0800

    Edited the gap between navigational elements.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 export default function Navbar() {
   return (
 
-    <div className="mt-4 mb-16 flex items-center lg:flex-nowrap lg:justify-between flex-wrap justify-center">
+    <div className="mt-4 mb-16 flex items-center lg:flex-nowrap lg:justify-between flex-wrap justify-center py-4 border-black/10 border-b-1 shadow-[0_0px_4px_rgba(0,0,0,0.02)]">
     <Button px="px-8 text-md font-semibold lg:mb-0 mb-2">PRINCESS GIRON</Button>
     <NavLinks/>
     </div>
