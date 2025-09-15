0dc059aac74e23e39ec10aecd1318652dfe637e4
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-15 20:05:40 +0800

    Changed the max width and height of pictures.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,7 +8,7 @@ export default function Education() {
         {education.map((educ) =>
                 <div className="flex w-full mb-4 border border-gray-400/50 rounded-md p-4 bg-white hover:drop-shadow-gray-400/30 hover:drop-shadow-lg hover:translate-y-1">
                     <div className="flex flex-col justify-center">
-                    <PictureHolder imgSrc={educ.url} width="max-w-[150px] max-h-[300px] mr-8"></PictureHolder>
+                    <PictureHolder imgSrc={educ.url} width="max-w-[150px] h-[200px] mr-8"></PictureHolder>
                     </div>
                     <div className="flex flex-col my-auto">
                     <Header isHeader={false} misc=" text-left mb-2">{educ.name}</Header>
