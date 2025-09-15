b095d56de96ab30b2c2606dd6910bcf1b41a2f56
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-15 20:06:32 +0800

    Replaced cert with skills.

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -3,26 +3,26 @@ import Header from "../components/Header"
 import Subheader from "../components/Subheader"
 import Organizations from "../components/Organizations"
 import Education from "../components/Education"
-import Certification from "../components/Certification"
 import { useState } from "react"
+import Skills from "../components/Skills"
 export default function Experience() {
     const [org, setOrg] = useState(true)
     const [educ, setEduc] = useState(false)
-    const [cert, setCert] = useState(false)
+    const [skill, setSkill] = useState(false)
     function clickOrg() {
         setOrg(true)
         setEduc(false)
-        setCert(false)
+        setSkill(false)
     }
     function clickEduc() {
         setOrg(false)
         setEduc(true)
-        setCert(false)
+        setSkill(false)
     }
-    function clickCert() {
+    function clickSkill() {
         setOrg(false)
         setEduc(false)
-        setCert(true)
+        setSkill(true)
     }
     return (
         <>
@@ -32,10 +32,10 @@ export default function Experience() {
             <div className="flex lg:gap-x-16 gap-x-8 justify-center">
                 <Subheader misc="mb-8 hover:underline hover:underline-offset-4 active:underline" handleClick={()=> clickOrg()}>Organizations</Subheader>
                 <Subheader misc="mb-8 hover:underline hover:underline-offset-4 active:underline" handleClick={()=> clickEduc()}>Education</Subheader>
-                <Subheader misc="mb-8 hover:underline hover:underline-offset-4 focus:text-white" handleClick={()=> clickCert()}>Certifications</Subheader>
+                <Subheader misc="mb-8 hover:underline hover:underline-offset-4 focus:text-white" handleClick={()=> clickSkill()}>Skills</Subheader>
             </div>
             <div className="lg:max-w-6xl sm:max-w-xl">
-                <p>{org ? <Organizations/> : educ ? <Education/> : <Certification/>}</p>
+                <p>{org ? <Organizations/> : educ ? <Education/> : <Skills/>}</p>
             </div>
             <div>
 
