609003931a5e6130f8fa21ec310862a39902e6c5
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-20 08:23:25 +0800

    Edited projects, socials, and skills.

--- src/data.js
+++ src/data.js
@@ -6,6 +6,16 @@ import facebook from "./assets/facebook.png";
 import github from "./assets/github.png";
 import gmail from "./assets/gmail.png";
 import linkedin from "./assets/linkedin.png";
+import google from "./assets/google.png"
+import html from "./assets/html.png"
+import java from "./assets/java.png"
+import python from "./assets/python.jpg"
+import react from "./assets/react.png"
+import tableau from "./assets/tableau.png"
+import canva from "./assets/canva.png"
+import css from "./assets/css.png"
+import django from "./assets/django.png"
+import figma from "./assets/figma.png"
 export const organization = [
     {
         name: "Ateneo Gabay",
@@ -55,39 +65,167 @@ export const education = [
     },
 ]
 
-export const skills = [
-    {
-        name: "",
-        year: "",
-    },
-]
+export const skills = {
+  frontend: [
+    { name: "HTML", icon: html },
+    { name: "CSS", icon: css },
+    { name: "React JS", icon: react },
+  ],
+  backend: [
+    { name: "Python", icon: python },
+    { name: "Django", icon: django },
+    { name: "Java", icon: java },
+  ],
+  data: [
+    { name: "Tableau", icon: tableau },
+    { name: "Google Workspace", icon: google },
+  ],
+  design: [
+    { name: "Canva", icon: canva },
+    { name: "Figma", icon: figma },
+  ]
+};
+
 
 export const projects = [
     {
-        name: "",
-        description: "",
+        name: "Hobbysite",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula velit. In et nulla massa. Sed vel dui facilisis, luctus mauris at, consectetur enim. Quisque dapibus dapibus mi ac luctus. Nam nec nisi et ligula molestie tristique. Suspendisse potenti. Nunc in feugiat metus. Etiam sit amet accumsan sem, ac molestie ipsum. Fusce varius purus vel tortor feugiat, in rutrum odio pretium. Integer ipsum ligula, tempus id nibh a, faucibus vestibulum enim. Sed faucibus mattis urna, tempor porta ante efficitur in. Donec dignissim vel diam nec posuere.",
+        img:"https://garden.spoonflower.com/c/5439145/p/f/m/R8RWIFy4JLa-w7-0qGiHV5HyNOloRHtYiGsZjRFa2gI0957w9KMAVhld/5439145-summer-doodle-geometric-triangle-deer-unicorn-rainbow-cactus-flamingo-pineapple-on-grey-by-caja_design.jpg",
+        skills: [
+            {
+            language: 'HTML',
+            color: 'bg-[#dc2626] hover:bg-[#991b1b] text-white font-medium',
+            },
+            {
+            language: 'CSS',
+            color: 'bg-[#1e40af] hover:bg-[#1e3a8a] text-white font-medium',
+            },
+            {
+                language: 'Figma',
+                color: 'bg-[#7c3aed] hover:bg-[#5b21b6] text-white font-medium',
+            },
+            {
+            language: 'Django',
+            color: 'bg-[#14532d] hover:bg-[#052e16] text-white font-medium',
+            },
+        ],
         link: "",
-    }
+    },
+    {
+        name: "OSG Hub",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula velit. In et nulla massa. Sed vel dui facilisis, luctus mauris at, consectetur enim. Quisque dapibus dapibus mi ac luctus. Nam nec nisi et ligula molestie tristique. Suspendisse potenti. Nunc in feugiat metus. Etiam sit amet accumsan sem, ac molestie ipsum. Fusce varius purus vel tortor feugiat, in rutrum odio pretium. Integer ipsum ligula, tempus id nibh a, faucibus vestibulum enim. Sed faucibus mattis urna, tempor porta ante efficitur in. Donec dignissim vel diam nec posuere.",
+        img:"https://garden.spoonflower.com/c/5439145/p/f/m/R8RWIFy4JLa-w7-0qGiHV5HyNOloRHtYiGsZjRFa2gI0957w9KMAVhld/5439145-summer-doodle-geometric-triangle-deer-unicorn-rainbow-cactus-flamingo-pineapple-on-grey-by-caja_design.jpg",
+        skills: [
+            {
+            language: 'HTML',
+            color: 'bg-[#dc2626] hover:bg-[#991b1b] text-white font-medium',
+            },
+            {
+            language: 'CSS',
+            color: 'bg-[#1e40af] hover:bg-[#1e3a8a] text-white font-medium',
+            },
+        ],
+        link: "",
+    },
+    {
+        name: "Spotify Home",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula velit. In et nulla massa. Sed vel dui facilisis, luctus mauris at, consectetur enim. Quisque dapibus dapibus mi ac luctus. Nam nec nisi et ligula molestie tristique. Suspendisse potenti. Nunc in feugiat metus. Etiam sit amet accumsan sem, ac molestie ipsum. Fusce varius purus vel tortor feugiat, in rutrum odio pretium. Integer ipsum ligula, tempus id nibh a, faucibus vestibulum enim. Sed faucibus mattis urna, tempor porta ante efficitur in. Donec dignissim vel diam nec posuere.",
+        img:"https://garden.spoonflower.com/c/5439145/p/f/m/R8RWIFy4JLa-w7-0qGiHV5HyNOloRHtYiGsZjRFa2gI0957w9KMAVhld/5439145-summer-doodle-geometric-triangle-deer-unicorn-rainbow-cactus-flamingo-pineapple-on-grey-by-caja_design.jpg",
+        skills: [
+            {
+            language: 'HTML',
+            color: 'bg-[#dc2626] hover:bg-[#991b1b] text-white font-medium',
+            },
+            {
+            language: 'CSS',
+            color: 'bg-[#1e40af] hover:bg-[#1e3a8a] text-white font-medium',
+            },
+        ],
+        link: "",
+    },
+    {
+        name: "Grow A Garden: A Data Story",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula velit. In et nulla massa. Sed vel dui facilisis, luctus mauris at, consectetur enim. Quisque dapibus dapibus mi ac luctus. Nam nec nisi et ligula molestie tristique. Suspendisse potenti. Nunc in feugiat metus. Etiam sit amet accumsan sem, ac molestie ipsum. Fusce varius purus vel tortor feugiat, in rutrum odio pretium. Integer ipsum ligula, tempus id nibh a, faucibus vestibulum enim. Sed faucibus mattis urna, tempor porta ante efficitur in. Donec dignissim vel diam nec posuere.",
+        img:"https://garden.spoonflower.com/c/5439145/p/f/m/R8RWIFy4JLa-w7-0qGiHV5HyNOloRHtYiGsZjRFa2gI0957w9KMAVhld/5439145-summer-doodle-geometric-triangle-deer-unicorn-rainbow-cactus-flamingo-pineapple-on-grey-by-caja_design.jpg",
+        skills: [
+            {
+            language: 'Google Sheets',
+            color: 'bg-[#15803d] hover:bg-[#166534] text-white font-medium',
+            },
+            {
+            language: 'Tableau',
+            color: 'bg-[#e25822] hover:bg-[#b23c17] text-white font-medium',
+            },
+            {
+            language: 'Canva',
+            color: 'bg-[#0d9488] hover:bg-[#115e59] text-white font-medium',
+            },
+        ],
+        link: "",
+    },
+    {
+        name: "Nike 2021 Sales Dashboard",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula velit. In et nulla massa. Sed vel dui facilisis, luctus mauris at, consectetur enim. Quisque dapibus dapibus mi ac luctus. Nam nec nisi et ligula molestie tristique. Suspendisse potenti. Nunc in feugiat metus. Etiam sit amet accumsan sem, ac molestie ipsum. Fusce varius purus vel tortor feugiat, in rutrum odio pretium. Integer ipsum ligula, tempus id nibh a, faucibus vestibulum enim. Sed faucibus mattis urna, tempor porta ante efficitur in. Donec dignissim vel diam nec posuere.",
+        img:"https://garden.spoonflower.com/c/5439145/p/f/m/R8RWIFy4JLa-w7-0qGiHV5HyNOloRHtYiGsZjRFa2gI0957w9KMAVhld/5439145-summer-doodle-geometric-triangle-deer-unicorn-rainbow-cactus-flamingo-pineapple-on-grey-by-caja_design.jpg",
+        skills: [
+            {
+            language: 'Kaggle',
+            color: 'bg-[#0284c7] hover:bg-[#0369a1] text-white font-medium',
+            },
+            {
+            language: 'Tableau Prep',
+            color: 'bg-[#be123c] hover:bg-[#881337] text-white font-medium',
+            },
+            {
+            language: 'Tableau',
+            color: 'bg-[#e25822] hover:bg-[#b23c17] text-white font-medium',
+            },
+            
+        ],
+        link: "",
+    },
+    {
+        name: "Either Java or New Project",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula velit. In et nulla massa. Sed vel dui facilisis, luctus mauris at, consectetur enim. Quisque dapibus dapibus mi ac luctus. Nam nec nisi et ligula molestie tristique. Suspendisse potenti. Nunc in feugiat metus. Etiam sit amet accumsan sem, ac molestie ipsum. Fusce varius purus vel tortor feugiat, in rutrum odio pretium. Integer ipsum ligula, tempus id nibh a, faucibus vestibulum enim. Sed faucibus mattis urna, tempor porta ante efficitur in. Donec dignissim vel diam nec posuere.",
+        img:"https://garden.spoonflower.com/c/5439145/p/f/m/R8RWIFy4JLa-w7-0qGiHV5HyNOloRHtYiGsZjRFa2gI0957w9KMAVhld/5439145-summer-doodle-geometric-triangle-deer-unicorn-rainbow-cactus-flamingo-pineapple-on-grey-by-caja_design.jpg",
+        skills: [
+            {
+            language: 'Kaggle',
+            color: 'bg-[#0284c7] hover:bg-[#0369a1] text-white font-medium',
+            },
+            {
+            language: 'Tableau Prep',
+            color: 'bg-[#be123c] hover:bg-[#881337] text-white font-medium',
+            },
+            {
+            language: 'Tableau',
+            color: 'bg-[#e25822] hover:bg-[#b23c17] text-white font-medium',
+            },
+            
+        ],
+        link: "",
+    },
 ]
 export const socials = [
     {
         social: "gironprincessmayb@gmail.com",
-        link: "",
+        link: "https://mail.google.com/mail/?view=cm&fs=1&to=gironprincessmayb@gmail.com&su=Portfolio%20Inquiry&body=Hello%20Princess,%0A%0AI%20came%20across%20your%20portfolio%20and...",
         icon: gmail,
     },
     {
         social: "youth-of-may",
-        link: "",
+        link: "https://github.com/youth-of-may",
         icon: github,
     },
     {
         social: "Princess May Giron",
-        link: "",
+        link: "https://www.linkedin.com/in/princess-may-giron-46197b279/",
         icon: linkedin,
     },
     {
         social: "Princess May Giron",
-        link: "",
+        link: "https://www.facebook.com/princessmaygiron",
         icon: facebook,
     }
 ]
