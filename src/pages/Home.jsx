1fa4ce9283c9ebe48c013240bb52005c36084765
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-23 11:49:33 +0800

    Added headshot.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,13 @@ import Header from "../components/Header"
 import Subheader from "../components/Subheader"
 import { socials } from "../data"
 import Button from "../components/Button"
+import pic from "../assets/homepic.jpg"
 export default function Home() {
-    const url = "https://upload.wikimedia.org/wikipedia/commons/b/b5/Baby.tux.sit-black-800x800.png"
     return (
         
         <>
         <div className="flex lg:justify-around lg:gap-x-4 justify-center flex-wrap">
-            <img src={url} className="rounded-full lg:w-md lg:mb-1 w-[250px] mb-4"/>
+            <img src={pic} className="rounded-full lg:w-md lg:mb-1 w-[250px] mb-4"/>
             <div className="flex flex-col justify-center text-center">
                 <Subheader size="text-md" misc="mb-1" color="text-black/50" weight="font-semibold">Hello I'm</Subheader>
                 <Header isHeader={true} misc="mb-2">Princess May Giron</Header>
