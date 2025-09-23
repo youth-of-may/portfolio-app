cc25cc1ba7b2a18ca78222a187e2edbe0ffed23e
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-23 20:15:06 +0800

    Added speed insights.

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>
 );
