e04375bfe02004b8269c3a3390f62afbab2a409b
youth-of-may <princess.giron@obf.ateneo.edu>
2025-09-23 12:03:01 +0800

    Increased the gap between items.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
             <Header isHeader={true} misc="mb-1">Contact Me</Header>
             <Subheader misc="mb-16" color="text-black/60">Get In Touch</Subheader>
             <div className="flex flex-row justify-center w-full">
-            <div className="flex flex-wrap mb-4 lg:gap-x-8 py-8 gap-x-2 border border-gray-400/50 rounded-xl p-4 bg-white hover:drop-shadow-gray-400/30 hover:drop-shadow-lg">
+            <div className="flex flex-wrap mb-4 lg:gap-x-8 py-8 gap-x-2 gap-y-2 border border-gray-400/50 rounded-xl p-4 bg-white hover:drop-shadow-gray-400/30 hover:drop-shadow-lg">
                 {socials.map((soc) => 
                     <div className="flex lg:w-auto gap-y-8 w-full justify-center items-center">
                         <img className="lg:w-[50px] lg:h-[50px] w-[35px] h-[35px]" src={soc.icon}/>
